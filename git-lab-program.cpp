c59d83b426f113eeb3388605e509c6a190194163
Zach Bernens <zb162003@ohio.edu>
2019-05-16 16:40:03 -0400

    Initial commit

---
+++ git-lab-program.cpp
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Zach Bernens
+ *        Date: 5/16/2019
+ * Description: This program outputs a message
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
